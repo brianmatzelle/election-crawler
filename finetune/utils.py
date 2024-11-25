35ef23cc6acabf64616858bdab0c9f822b57d9d7
Brian Matzelle <bmatzel1@binghamton.edu>
2024-11-25 16:17:47 -0500

    destiny+hasan dataset created

--- finetune/utils.py
+++ finetune/utils.py
@@ -11,7 +11,7 @@ def get_unique_filename(file_path):
     new_file_path = file_path
 
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    
+
     while os.path.exists(new_file_path):
         new_file_path = f"{base}({counter}){ext}"
         counter += 1
@@ -26,3 +26,38 @@ def dump_unique_json(data, file_path):
         json.dump(data, f, indent=4)
     print(f"Data saved to {new_file_path}")
     return new_file_path
+
+def to_k(size: int, logger):
+  if size < 1000:
+    logger.error(f"{size} rows is too small to train a model with, exiting")
+    exit(1)
+  elif size < 1000000:
+    logger.info(f"{size // 1000}k rows")
+    return f"{size // 1000}k"
+  else:
+    logger.info(f"{size // 1000000}.{size // 100000 % 10}M rows")
+    return f"{size // 1000000}.{size // 100000 % 10}M"
+  
+
+def is_comment_valid(comment) -> tuple[bool, str]:
+  if (comment.startswith('!') and comment != "!") or 'http:' in comment or 'https:' in comment:
+    return False, f"Comment is a bot call or contains a link: {comment}"
+  match comment:
+    case '[deleted]':
+      return False, "Comment was deleted"
+    case '[removed]':
+      return False, "Comment was removed"
+    case _:
+      return True, ""
+    
+def is_post_valid(post_row) -> tuple[bool, str]:
+  if post_row['selftext'].startswith('#'):
+    return False, f"Post is a bot: {post_row['selftext']}"
+  if post_row['num_comments'] == 0:
+    return False, "Post has no comments"
+  return True, ""
+
+def get_conversations_file(name: str, size: str) -> str: 
+  path = f'data/datasets/{name}-conversations-{size}.jsonl'
+  unique_path = get_unique_filename(path)
+  return unique_path
