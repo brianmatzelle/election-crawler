aaebada3836b09f75ce40afd87b8a14435851968
Brian Matzelle <bmatzel1@binghamton.edu>
2024-11-25 14:20:16 -0500

    proccess raw data done, saved locally. Working on a hasan model now

--- finetune/utils.py
+++ finetune/utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 def get_unique_filename(file_path):
     """
@@ -8,7 +9,20 @@ def get_unique_filename(file_path):
     base, ext = os.path.splitext(file_path)
     counter = 1
     new_file_path = file_path
+
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    
     while os.path.exists(new_file_path):
         new_file_path = f"{base}({counter}){ext}"
         counter += 1
+    return new_file_path
+
+def dump_unique_json(data, file_path):
+    """
+    Dumps json to a file, appending a number to the file name if it already exists.
+    """
+    new_file_path = get_unique_filename(file_path)
+    with open(new_file_path, 'w') as f:
+        json.dump(data, f, indent=4)
+    print(f"Data saved to {new_file_path}")
     return new_file_path
